87ee6826d5385f6ac41e6d65a77074553158dd57
Marcin Kostrzewa <marckostrzewa@gmail.com>
2025-06-21 17:00:07 +0200

    unify with nr

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use ark_ec::CurveGroup;
+use ark_ec::{AffineRepr, CurveGroup};
 use ark_ff::{
     BigInteger, Field, PrimeField, UniformRand,
     field_hashers::{DefaultFieldHasher, HashToField},
 };
 use ark_std::rand::thread_rng;
+use blake2::{Blake2s256, Digest};
 use sha2::Sha256;
 
 struct Key<G: CurveGroup> {
@@ -38,34 +39,47 @@ enum Message<G: CurveGroup> {
     Curve(G),
 }
 
-impl<G: CurveGroup> Message<G> {
+impl<G: CurveGroup> Message<G>
+where
+    G::BaseField: PrimeField,
+{
     fn to_bytes(&self) -> Vec<u8> {
         match self {
-            Self::Field(value) => value.into_bigint().to_bytes_be(),
+            Self::Field(value) => value.into_bigint().to_bytes_le(),
             Self::Curve(point) => {
-                let mut uncompressed_bytes = Vec::new();
-                point
-                    .serialize_uncompressed(&mut uncompressed_bytes)
-                    .unwrap();
-
-                uncompressed_bytes
+                let mut x = point.into_affine().x().unwrap().into_bigint().to_bytes_le();
+                let mut y = point.into_affine().y().unwrap().into_bigint().to_bytes_le();
+                x.append(&mut y);
+                x
             }
         }
     }
 }
 
-fn hash<G: CurveGroup>(message: Vec<Message<G>>) -> G::ScalarField {
-    let hasher = <DefaultFieldHasher<Sha256> as HashToField<G::ScalarField>>::new(&[]);
+fn hash_to_field<F: PrimeField>(data: &[u8]) -> F {
+    let mut hasher = Blake2s256::new();
+    hasher.update(data);
+    let mut out = hasher.finalize();
+    out[31] = 0;
+    let out = F::from_le_bytes_mod_order(&out);
+    out
+}
+fn hash<G: CurveGroup>(message: Vec<Message<G>>) -> G::ScalarField
+where
+    G::ScalarField: PrimeField,
+    G::BaseField: PrimeField,
+{
     let preimage = message
         .iter()
-        .map(|m| m.to_bytes())
-        .flatten()
+        .flat_map(|m| m.to_bytes())
         .collect::<Vec<_>>();
-    let hashes: [G::ScalarField; 1] = hasher.hash_to_field(&preimage);
-    hashes[0]
+    hash_to_field(&preimage)
 }
 
-impl<G: CurveGroup> Key<G> {
+impl<G: CurveGroup> Key<G>
+where
+    G::BaseField: PrimeField,
+{
     fn new() -> Self {
         let mut rng = thread_rng();
         let x = G::ScalarField::rand(&mut rng);
@@ -101,7 +115,10 @@ impl<G: CurveGroup> Key<G> {
     // }
 }
 
-impl<G: CurveGroup> SigningToken<G> {
+impl<G: CurveGroup> SigningToken<G>
+where
+    G::BaseField: PrimeField,
+{
     fn deg_sign(&self, m: G::ScalarField) -> Signature<G> {
         let mut rng = thread_rng();
 
@@ -132,7 +149,10 @@ impl<G: CurveGroup> SigningToken<G> {
     // }
 }
 
-impl<G: CurveGroup> Signature<G> {
+impl<G: CurveGroup> Signature<G>
+where
+    G::BaseField: PrimeField,
+{
     fn verify(&self, m: G::ScalarField, vk: G, _bulletin_board: String) -> bool {
         //       R0 = Z0 + [-c0]X
         // => [r0]G = [z0]G - [c0 * x] G
@@ -148,7 +168,7 @@ impl<G: CurveGroup> Signature<G> {
             hash(vec![
                 Message::Field(self.theta.m0),
                 Message::Curve(vk), // [x]G
-                Message::Curve(R0) // [r0]G
+                Message::Curve(R0)  // [r0]G
             ])
         );
         assert_eq!(
@@ -179,4 +199,66 @@ fn test_random() {
 
     let delegated_signature = token.deg_sign(m);
     assert!(delegated_signature.verify(m, key.X, "".to_string()));
+}
+
+fn grumpkin_fr_to_nr_code(fr: ark_grumpkin::Fr) -> String {
+    let bi = fr.into_bigint().0;
+    let lo: u128 = (bi[0] as u128) | (bi[1] as u128) << 64;
+    let hi: u128 = (bi[2] as u128) | (bi[3] as u128) << 64;
+    format!("EmbeddedCurveScalar::new(0x{:x}, 0x{:x})", lo, hi)
+}
+
+fn grumpkin_point_to_nr_code(point: ark_grumpkin::Projective) -> String {
+    let aff = point.into_affine();
+    format!(
+        "EmbeddedCurvePoint {{x: {}, y: {}, is_infinite: false}}",
+        aff.x().unwrap(),
+        aff.y().unwrap()
+    )
+    .to_string()
+}
+
+fn grumpkin_sig_to_nr_code(signature: &Signature<ark_grumpkin::Projective>) -> String {
+    let mut s = String::new();
+    s.push_str("    let sigma = Sigma {\n");
+    s.push_str(&format!(
+        "        c0: {},\n",
+        grumpkin_fr_to_nr_code(signature.sigma.c0)
+    ));
+    s.push_str(&format!(
+        "        c1: {},\n",
+        grumpkin_fr_to_nr_code(signature.sigma.c1)
+    ));
+    s.push_str(&format!(
+        "        z1: {}\n",
+        grumpkin_fr_to_nr_code(signature.sigma.z1)
+    ));
+    s.push_str("    };\n");
+    s.push_str("    let theta = Theta {\n");
+    s.push_str(&format!(
+        "        m0: {},\n",
+        grumpkin_fr_to_nr_code(signature.theta.m0)
+    ));
+    s.push_str(&format!(
+        "        Z0: {}\n",
+        grumpkin_point_to_nr_code(signature.theta.Z0)
+    ));
+    s.push_str("    };\n");
+    s.push_str("    let sig = Signature { sigma, theta };\n");
+    s
+}
+
+#[test]
+fn test_grumpkin() {
+    use ark_grumpkin::{Fr, Projective};
+    let mut rng = thread_rng();
+    let m = Fr::rand(&mut rng);
+    let key = Key::<Projective>::new();
+    let signature = key.sign(m);
+
+    println!("    let vk = {};", grumpkin_point_to_nr_code(key.X));
+    println!("    let msg = {};", grumpkin_fr_to_nr_code(m));
+    println!("{}", grumpkin_sig_to_nr_code(&signature));
+
+    assert!(signature.verify(m, key.X, "".to_string()));
 }
