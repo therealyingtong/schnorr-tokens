12c13286cda1ad2c4ce0d0d47879aa1b536de69a
Nicolas Mohnblatt <nmohnblatt@protonmail.com>
2025-06-21 16:14:38 +0200

    document the interface

--- src/lib.rs
+++ src/lib.rs
@@ -11,25 +11,39 @@ pub enum Error {
     UseOfRevokedToken,
 }
 
+/// Interface for a proxy signature scheme as defined in [AN23](https://eprint.iacr.org/2023/833).
 pub trait ProxySignature {
+    /// Public parameters,
     type Parameters;
+    /// Private signing key.
     type SigningKey;
+    /// Public verification key.
     type VerificationKey;
+    /// The type of messages that can be signed.
     type Message;
+    /// The type of policy that can be used to restrict the delegation.
     type Policy;
+    /// The type of delegation specification that defines how the delegation should be performed.
     type DelegationSpec;
+    /// Delegation information used by a proxy to sign messages on behalf of the delegator. Anyone with this information can sign messages on behalf of the delegator; treat with care!
     type DelegationInfo;
+    /// Revocation keys.
     type RevocationKey;
+    /// A publicly accessible, append-only list of revoked delegation information.
     type RevocationState;
+    /// A signature.
     type Signature;
 
+    /// Generates the public parameters for the proxy signature scheme.
     fn setup<R: Rng>(rng: &mut R) -> Result<Self::Parameters, Error>;
 
+    /// Generate a signing and verifying key pair.
     fn keygen<R: Rng>(
         rng: &mut R,
         parameters: &Self::Parameters,
     ) -> Result<(Self::SigningKey, Self::VerificationKey), Error>;
 
+    /// Sign using the signing key.
     fn sign<R: Rng>(
         rng: &mut R,
         parameters: &Self::Parameters,
@@ -38,6 +52,7 @@ pub trait ProxySignature {
         policy: Option<&Self::Policy>,
     ) -> Result<Self::Signature, Error>;
 
+    /// Generate delegation information for the proxy and a revocation key to be kept by the delegator.
     fn delegate<R: Rng>(
         rng: &mut R,
         parameters: &Self::Parameters,
@@ -45,6 +60,7 @@ pub trait ProxySignature {
         deg_spec: &Self::DelegationSpec,
     ) -> Result<(Self::DelegationInfo, Self::RevocationKey), Error>;
 
+    /// The proxy signer can use the delegation information to sign messages on behalf of the delegator.
     fn delegated_sign<R: Rng>(
         rng: &mut R,
         parameters: &Self::Parameters,
@@ -52,13 +68,15 @@ pub trait ProxySignature {
         message: &Self::Message,
     ) -> Result<Self::Signature, Error>;
 
+    /// The delegator can revoke the delegation by providing the revocation key and updating the revocation state.
     fn revoke(
         parameters: &Self::Parameters,
         delegation_info: &Self::DelegationInfo,
         rev_key: &Self::RevocationKey,
-        rev_state: &mut Self::RevocationState
+        rev_state: &mut Self::RevocationState,
     ) -> Result<(), Error>;
 
+    /// Verify the signature against the message and the verification key; update the revocation state if verification succeeds.
     fn verify(
         parameters: &Self::Parameters,
         vk: &Self::VerificationKey,
